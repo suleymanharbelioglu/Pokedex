e9bcf6297fb27c795b6837c6294cf26bf6d03896
suleymanharbelioglu <73783332+suleymanharbelioglu@users.noreply.github.com>
2024-10-24 18:02:35 +0300

    project folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,15 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      theme: ThemeData.dark().copyWith(
+        textTheme: GoogleFonts.lateefTextTheme(),
       ),
+      debugShowCheckedModeBanner: false,
+      title: 'Pokemon List',
+      home: HomePage(),
     );
   }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/app_title.dart';
+import 'package:flutter_application_1/widgets/pokemon_list.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          AppTitle(),
+          Expanded(
+            child: PokemonList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
